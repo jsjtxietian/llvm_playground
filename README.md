2a8c2247a582e9dc54c5365dd7b04d93266e232c
jsjtxietian <jsjtxietian@outlook.com>
2022-11-20 22:38:17 +0800

    lexer

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # llvm_playground
 
 《Learn LLVM 12》
+
+calc:
+with a, b: a * (4 + b)
+
+calc : ("with" ident ("," ident)* ":")? expr ;
+expr : term (( "+" | "-" ) term)* ;
+term : factor (( "*" | "/") factor)* ;
+factor : ident | number | "(" expr ")" ;
+ident : ([a-zAZ])+ ;
+number : ([0-9])+ ;
--- src/Lexer.h
+++ src/Lexer.h
@@ -2,6 +2,9 @@
 #define LEXER_H
 
 #include "llvm/ADT/StringRef.h"
+
+// provides read-only access to a block of memory, filled with the content of a file.
+// On request, a trailing zero character ('\x00') is added to the end of the buffer.
 #include "llvm/Support/MemoryBuffer.h"
 
 class Lexer;
@@ -11,6 +14,7 @@ class Token {
 
 public:
   enum TokenKind : unsigned short {
+    // end of input
     eoi,
     unknown,
     ident,
@@ -32,14 +36,10 @@ private:
 
 public:
   TokenKind getKind() const { return Kind; }
-  llvm::StringRef getText() const {
-    return Text;
-  }
+  llvm::StringRef getText() const { return Text; }
 
   bool is(TokenKind K) const { return Kind == K; }
-  bool isOneOf(TokenKind K1, TokenKind K2) const {
-    return is(K1) || is(K2);
-  }
+  bool isOneOf(TokenKind K1, TokenKind K2) const { return is(K1) || is(K2); }
   template <typename... Ts>
   bool isOneOf(TokenKind K1, TokenKind K2, Ts... Ks) const {
     return is(K1) || isOneOf(K2, Ks...);
@@ -59,7 +59,6 @@ public:
   void next(Token &token);
 
 private:
-  void formToken(Token &Result, const char *TokEnd,
-                 Token::TokenKind Kind);
+  void formToken(Token &Result, const char *TokEnd, Token::TokenKind Kind);
 };
 #endif
